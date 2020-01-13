bc2c29fc3d554d9d230ad310127e9589bfa5648f
Szocs Gabor <szocs.gaobr@progmedical.eu>
2020-01-13 16:42:24 +0100

    masodik

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main {
 
+    public static int db = 0;
+    public static ArrayList<Vizsga> adatok = new ArrayList<>();
 
     public static void main(String[] args) {
 
+        Beolvas("sikeres.csv", "sikertelen.csv");
+    }
 
 
+    private static void Beolvas(String file1, String file2) {
+        try {
+            BufferedReader be1 = new BufferedReader(new FileReader(file1));
+            BufferedReader be2 = new BufferedReader(new FileReader(file2));
+            String sor1 = be1.readLine();
+            String sor2 = be2.readLine();
+            while ((sor1 = be1.readLine()) != null) {
+                sor2 = be2.readLine();
+                String[] sikeres = sor1.split(";");
+                String[] sikertelen = sor2.split(";");
+                int[] x= new int [sikeres.length-1];
+                int[] y= new int [sikertelen.length-1];
+                for (int i = 0; i <sikeres.length ; i++) {
+                    x[i] = Integer.parseInt(sikeres[i]);
+                    y[i] = Integer.parseInt(sikertelen[i]);
+                }
+
+                adatok.add(new Vizsga(sikeres[0],x,y));
+
+            }
 
 
-
-
-
-
-
-
-
-
-
-
-
+        } catch (IOException error) {
+            System.out.println("Hiba az állomány beolvasása során!");
+        }
 
     }
 }
---
+++ src/Vizsga.java
@@ -0,0 +1,41 @@
+public class Vizsga {
+
+    public String getNyelv() {
+        return nyelv;
+    }
+
+    public Vizsga() {
+    }
+
+    public Vizsga(String nyelv, int[] sikeres, int[] sikertelen) {
+        this.nyelv = nyelv;
+        this.sikeres = sikeres;
+        this.sikertelen = sikertelen;
+    }
+
+    public void setNyelv(String nyelv) {
+        this.nyelv = nyelv;
+    }
+
+    public int[] getSikeres() {
+        return sikeres;
+    }
+
+    public void setSikeres(int[] sikeres) {
+        this.sikeres = sikeres;
+    }
+
+    public int[] getSikertelen() {
+        return sikertelen;
+    }
+
+    public void setSikertelen(int[] sikertelen) {
+        this.sikertelen = sikertelen;
+    }
+
+    private String nyelv;
+    private int[] sikeres;
+    private int[] sikertelen;
+
+
+}
