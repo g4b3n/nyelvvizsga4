072809b85e9988917526ace235adcd07cf977d4f
Szocs Gabor <szocs.gaobr@progmedical.eu>
2020-01-14 11:13:46 +0100

    hetfoi_befejezes

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,23 @@ import java.util.Scanner;
 public class Main {
     public static int db = 0;
     public static ArrayList<Vizsga> adatok = new ArrayList<>();
-    public int ev = 0;
+    public static int ev = 0;
     public static int[] vizsgaszam;
-    public static String[] nyelvneve;
+    public static int[] sikertelen;
+    public static int[] sikeres;
+    public static String[] nyelvek;
 
     public static void main(String[] args) {
         Beolvas("sikeres.csv", "sikertelen.csv");
         Nepszeru();
         System.out.println("3. feladat");
         System.out.println("Vizsgálandó év: " + evbe());
-        legnagyobbSikertelen();
+        legnagyobbSikertelen(); //Ez már nem működik!!
     }
 
     private static void legnagyobbSikertelen() {
         String az = "";
-        int index;
+        int index = 0;
         switch (ev) {
             case 2009:
                 index = 0;
@@ -54,78 +56,86 @@ public class Main {
                 index = 9;
                 break;
         }
-
-
-    }
-
-    private static int evbe() {
-        Scanner sc = new Scanner(System.in);
-        do {
-            System.out.println("Adjon meg egy évet: ");
-            ev = sc.nextInt();
-        } while (ev < 2008 || ev > 2019);
-        return ev;
-    }
-
-    private static void Nepszeru() {
-        vizsgaszam = new int[adatok.size()]; // Itt tároljuk összesítve a sikeres és sikertelen vizsgák számát, nyelvenként
-        nyelvek = new String[adatok.size()];
-
-        for (int i = 0; i < vizsgaszam.length; i++) { //Listában lévő adatokon megy végig - nyelveken megy végig
-            int osszeg = 0;
-            int sikeres[] = adatok.get(i).getSikeres();
-            int sikertelen[] = adatok.get(i).getSikertelen();
-            for (int j = 0; j < adatok.get(i).getSikeres().length; j++) { //éveken megy végig
-                osszeg += adatok.get(i).getSikeres()[j];
-            }
-            vizsgaszam[i] = osszeg;
-        }
+        double[] aranyok = new double[adatok.size()];
         for (int i = 0; i < adatok.size(); i++) {
-            nyelvek[i] = adatok.get(i).getNyelv();
-        }
-        for (int i = 0; i < adatok.size(); i++) {
-            for (int j = 0; j < adatok.size(); j++) {
-                if (vizsgaszam[i] > vizsgaszam[j]) {
-                    int seged = vizsgaszam[i];
-                    vizsgaszam[i] = vizsgaszam[j];
-                    vizsgaszam[j] = seged;
-                    String nev = nyelvek[i];
-                    nyelvek[i] = nyelvek[j];
-                    nyelvek[j] = nev;
-                }
-            }
-        }
-        for (int i = 0; i > adatok.size() - 3; i--) {
-            System.out.println("\t" + nyelvek[i]);
-        }
-    }
 
-    private static void Beolvas(String file1, String file2) {
-        try {
-            BufferedReader be1 = new BufferedReader(new FileReader(file1));
-            BufferedReader be2 = new BufferedReader(new FileReader(file2));
-            String sor1 = be1.readLine();
-            String sor2 = be2.readLine();
-            while ((sor1 = be1.readLine()) != null) {
-                sor2 = be2.readLine();
-                String[] sikeres = sor1.split(";");
-                String[] sikertelen = sor2.split(";");
-                int[] x = new int[sikeres.length - 1];
-                int[] y = new int[sikertelen.length - 1];
-                for (int i = 1; i < sikeres.length; i++) {
-                    x[i - 1] = Integer.parseInt(sikeres[i]);
-                    y[i - 1] = Integer.parseInt(sikertelen[i]);
-                }
-                adatok.add(new Vizsga(sikeres[0], x, y));
-                //System.out.println(adatok.get(db)); //beolvasás ellenőrzése!
-                db++;
-            }
-        } catch (IOException error) {
-            System.out.println("Hiba az állomány beolvasása során!");
+            aranyok[i] = (double) sikertelen[index] / sikeres[index] + sikertelen[index];
+            System.out.println(aranyok[i]);
         }
-
     }
 }
+
+
+    }
+
+private static int evbe(){
+        Scanner sc=new Scanner(System.in);
+        do{
+        System.out.println("Adjon meg egy évet: ");
+        ev=sc.nextInt();
+        }while(ev< 2008||ev>2019);
+        return ev;
+        }
+
+private static void Nepszeru(){
+        vizsgaszam=new int[adatok.size()]; // Itt tároljuk összesítve a sikeres és sikertelen vizsgák számát, nyelvenként
+        nyelvek=new String[adatok.size()];
+
+        for(int i=0;i<vizsgaszam.length;i++){ //Listában lévő adatokon megy végig - nyelveken megy végig
+        int osszeg=0;
+        int sikeres[]=adatok.get(i).getSikeres();
+        int sikertelen[]=adatok.get(i).getSikertelen();
+        for(int j=0;j<adatok.get(i).getSikeres().length;j++){ //éveken megy végig
+        osszeg+=adatok.get(i).getSikeres()[j];
+        }
+        vizsgaszam[i]=osszeg;
+        }
+        for(int i=0;i<adatok.size();i++){
+        nyelvek[i]=adatok.get(i).getNyelv();
+        }
+        for(int i=0;i<adatok.size();i++){
+        for(int j=0;j<adatok.size();j++){
+        if(vizsgaszam[i]>vizsgaszam[j]){
+        int seged=vizsgaszam[i];
+        vizsgaszam[i]=vizsgaszam[j];
+        vizsgaszam[j]=seged;
+        String nev=nyelvek[i];
+        nyelvek[i]=nyelvek[j];
+        nyelvek[j]=nev;
+        }
+        }
+        }
+        for(int i=0;i>adatok.size()-3;i--){
+        System.out.println("\t"+nyelvek[i]);
+        }
+        }
+
+private static void Beolvas(String file1,String file2){
+        try{
+        BufferedReader be1=new BufferedReader(new FileReader(file1));
+        BufferedReader be2=new BufferedReader(new FileReader(file2));
+        String sor1=be1.readLine();
+        String sor2=be2.readLine();
+        while((sor1=be1.readLine())!=null){
+        sor2=be2.readLine();
+        String[]sikeres=sor1.split(";");
+        String[]sikertelen=sor2.split(";");
+        int[]x=new int[sikeres.length-1];
+        int[]y=new int[sikertelen.length-1];
+        for(int i=1;i<sikeres.length;i++){
+        x[i-1]=Integer.parseInt(sikeres[i]);
+        y[i-1]=Integer.parseInt(sikertelen[i]);
+        }
+        adatok.add(new Vizsga(sikeres[0],x,y));
+        //System.out.println(adatok.get(db)); //beolvasás ellenőrzése!
+        db++;
+        }
+        }catch(IOException error){
+        System.out.println("Hiba az állomány beolvasása során!");
+        }
+
+        }
+        }
 
 
 
